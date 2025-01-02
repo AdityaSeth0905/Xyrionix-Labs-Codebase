6fd31916bd5d70e0457ac499c780034fa0098760
Aditya seth <adityaseth936@gmail.com>
2025-01-02 22:25:32 +0530

    cleaned useless files from the directory

--- smtp-server/src/models/email_model.py
+++
@@ -1,9 +0,0 @@
-
-from pydantic import BaseModel
-from typing import List, Optional
-
-class Email(BaseModel):
-    to: str
-    subject: str
-    body: str
-    attachments: Optional[List[str]] = None
